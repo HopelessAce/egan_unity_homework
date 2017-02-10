94c6fe0f1b7df4f3e5d5b99e04a80d7102c05170
Amanda Egan <Amanda.c.egan@gmail.com>
2017-02-10 14:06:41 -0800

    Homework_4

---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		//string helloworld = "Hello World";
+		//print (helloworld);
+		//transform.position = new Vector3(10,10,10);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//The slashes tell it to comment out lines
+		//moves object up 0.1 frame
+		//transform.position = new Vector3 (transform.position.x, transform.position.y + 1, transform.position.z);
+	}
+
+
+}
