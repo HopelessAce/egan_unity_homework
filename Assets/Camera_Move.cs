89d2f8f2ccc35687d789f532479b76c135bedc05
Amanda Egan <Amanda.c.egan@gmail.com>
2017-03-24 13:22:19 -0700

    Homework 9

---
+++ Assets/Camera_Move.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera_Move : MonoBehaviour {
+
+    public GameObject target;
+    public float xcorrection = 1;
+    public float zcorrection = 1;
+    public float lerpspeed = 1;
+
+    void Update()
+    {
+        Vector3 moveTarget = new Vector3((target.transform.position.x + xcorrection), transform.position.y, (target.transform.position.z + zcorrection));
+        transform.position = Vector3.Lerp(transform.position, moveTarget, Time.deltaTime * lerpspeed);
+    
+}
+}
---
+++ Assets/NewBehaviourScript1.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript1 : MonoBehaviour {
+
+    public Rigidbody rb;
+    public float hoverforce = 100;
+    public float forwardforce = 2;
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+	}
+	
+	
+	void FixedUpdate ()
+    {
+        Vector3 forward = transform.TransformDirection(Vector3.up * -1) * 10;
+        Debug.DrawRay(transform.position, forward, Color.green);
+
+        RaycastHit hit;
+
+        Ray ray = new Ray(transform.position, transform.up * -1);
+
+        if (Physics.Raycast(ray, out hit))
+        {
+            print(hit.distance);
+            rb.AddRelativeForce(Vector3.up * hoverforce/Mathf.Exp(hit.distance));
+        }
+
+        rb.AddRelativeTorque(Vector3.up * hoverforce * Input.GetAxis("Horizontal"));
+        rb.AddRelativeTorque(Vector3.left * forwardforce * Input.GetAxis("Vertical"));
+	}
+}
